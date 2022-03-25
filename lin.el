ea174a53f28009ef567f63ca11baf66b4bd707ee
Protesilaos Stavrou <info@protesilaos.com>
2022-03-25 07:14:44 +0200

    Format the Commentary a bit better

--- lin.el
+++ lin.el
@@ -26,26 +26,27 @@
 ;;; Commentary:
 ;;
 ;; Lin is a stylistic enhancement for Emacs' built-in `hl-line-mode'.
-;; It remaps the `hl-line face (or equivalent) buffer-locally to a style
-;; that is optimal for major modes where line selection is the primary
-;; mode of interaction.
+;; It remaps the `hl-line' face (or equivalent) buffer-locally to a
+;; style that is optimal for major modes where line selection is the
+;; primary mode of interaction.
 ;;
 ;; The idea is that `hl-line-mode' cannot work equally well for contexts
 ;; with competing priorities: (i) line selection, or (ii) simple line
 ;; highlight.  In the former case, the current line needs to be made
-;; prominent because it carries a specific meaning of some significance in
-;; the given context: the user has to select a line.  Whereas in the latter
-;; case, the primary mode of interaction does not revolve around the line
-;; highlight itself: it may be because the focus is on editing text or
-;; reading through the buffer's contents, so the current line highlight is
-;; more of a reminder of the point's location on the vertical axis.
+;; prominent because it carries a specific meaning of some significance
+;; in the given context: the user has to select a line.  Whereas in the
+;; latter case, the primary mode of interaction does not revolve around
+;; the line highlight itself: it may be because the focus is on editing
+;; text or reading through the buffer's contents, so the current line
+;; highlight is more of a reminder of the point's location on the
+;; vertical axis.
 ;;
-;; `lin-mode' enables `hl-line-mode' by adding it to every hook specified
-;; in the user option `lin-mode-hooks'.  Users are advised to configure
-;; that variable with `customize-set-variable', or the Custom UI, or
-;; equivalent as it has a function which automatically sets up Lin.  Those
-;; who prefer to set values with `setq' must handle the process manually,
-;; by using the `lin-setup' function.
+;; `lin-mode' enables `hl-line-mode' by adding it to every hook
+;; specified in the user option `lin-mode-hooks'.  Users are advised to
+;; configure that variable with `customize-set-variable', or the Custom
+;; UI, or equivalent as it has a function which automatically sets up
+;; Lin.  Those who prefer to set values with `setq' must handle the
+;; process manually, by using the `lin-setup' function.
 ;;
 ;; Users can selected their preferred style by customizing the user
 ;; option `lin-face'.  Options include the faces `lin-red', `lin-green',
